fbd89e5886ca2a923366098b4b430c13f7e0ffdf
Chris Carman <ccarmander@gmail.com>
2021-01-11 13:21:03 -0500

    recommitting entire homework at once due to loss of files error

---
+++ models/transaction.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose;
+
+const transaction = new Schema({
+    name: { 
+        type: String,
+        trim: true,
+        required: 'Enter a name for your transaction',
+    },
+    value: {
+        type: Number,
+        required: 'Enter the transaction amount',
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
